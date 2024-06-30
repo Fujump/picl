2e0d432278fbe33c9efd9638f050f61c63aca43a
Fujump <12111214@mail.sustech.edu.cn>
2024-06-30 12:01:17 +0000

    gen

--- main_gen.py
+++ main_gen.py
@@ -4,6 +4,8 @@ import os
 import pandas as pd
 
 import torch
+from tqdm import tqdm
+import outlines
 import argparse
 import evaluate
 # from accelerate import init_empty_weights, load_checkpoint_and_dispatch
@@ -14,6 +16,54 @@ from model import get_model, get_tokenizer
 from algorithms import get_retriever
 from inference import get_inferencer
 from common import get_prompt_label, extract_data, setup_seed, get_input, calculate_precision_recall, calculate_precision_recall_neg, delect_unavailable_word
+
+pretrained_model_dic = {
+        "gpt2":"/mnt/sharedata/ssd/users/hongfugao/model/Llama-2-7b-chat-hf",
+        "bert":"/mnt/sharedata/ssd/users/huq/model/bert-base-chinese",
+        "baichuan2":"/mnt/sharedata/ssd/users/huq/model/Baichuan2-7B-Chat",
+        "qwen1.5":"/mnt/sharedata/ssd/users/huq/model/Qwen1.5-7B-Chat",
+        "baichuan2_ft":"/mnt/sharedata/ssd/users/huq/model/Baichuan2-7B-Chat-ft-firefly-merged",
+        "qwen1.5_ft":"/mnt/sharedata/ssd/users/huq/model/Qwen1.5-7B-Chat-ft-firefly-merged",
+        "baichuan2_warmup":"/mnt/sharedata/ssd/users/huq/model/warmup_baichuan2/checkpoint-final-merged",
+        "qwen1.5_warmup":"/mnt/sharedata/ssd/users/huq/model/warmup_qwen1.5/checkpoint-final-merged",
+        "qwen1.5-14b":"/mnt/sharedata/ssd/users/huq/model/Qwen1.5-14B-Chat",
+        "qwen1.5-14b-ft":"/mnt/sharedata/ssd/users/huq/model/Qwen1.5-14B-Chat-ft",
+        "baichuan2_warmup_topk":"/mnt/sharedata/ssd/users/huq/model/baichuan2-warmup-topk/checkpoint-final-merged",
+        "qwen1.5_warmup_topk":"/mnt/sharedata/ssd/users/huq/model/qwen1.5-warmup-topk/checkpoint-final-merged",
+        "tinyllama":"/mnt/sharedata/ssd/users/huq/model/TinyLlama-1.1B-Chat-v1.0",
+        "llama3":"/mnt/sharedata/ssd/users/huq/model/Llama3-ChatQA-1.5-8B",
+        "llama3-c":"/mnt/sharedata/ssd/users/huq/model/Llama3-Chinese_v2",
+        "chatglm3":"/mnt/sharedata/ssd/users/huq/model/chatglm3-6b",
+        "internlm2-chat-7b":"/mnt/sharedata/ssd/users/huq/model/internlm2-chat-7b",
+        "Yi-1.5-9B-Chat":"/mnt/sharedata/ssd/users/huq/model/Yi-1.5-9B-Chat",
+        "Mistral-7B":"/mnt/sharedata/ssd/common/LLMs/Mistral-7B-Instruct-v0.3",
+        "Mistral-22B":"/mnt/sharedata/ssd/common/LLMs/Mixtral-8x22B-Instruct-v0.1",
+        "gemma-1.1-7b":"/mnt/sharedata/ssd/common/LLMs/gemma-1.1-7b-it",
+        "GPT-J-6B":"/mnt/sharedata/ssd/common/LLMs/GPT-J-6B",
+        "opt-6.7b":"/mnt/sharedata/ssd/common/LLMs/opt-6.7b",
+        "opt-13b":"/mnt/sharedata/ssd/common/LLMs/opt-13b",
+        "opt-30b":"/mnt/sharedata/ssd/common/LLMs/opt-30b",
+        "opt-66b":"/mnt/sharedata/ssd/common/LLMs/opt-66b",
+        "Qwen2-7B":"/mnt/sharedata/ssd/common/LLMs/Qwen2-7B",
+        "Qwen2-72B":"/mnt/sharedata/ssd/common/LLMs/Qwen2-72B",
+        "Qwen1.5-72B-Chat":"/mnt/sharedata/ssd/common/LLMs/Qwen1.5-72B-Chat",
+        "glm-4-9b-chat":"/mnt/sharedata/ssd/common/LLMs/glm-4-9b-chat",
+        "YI-1.5-9B":"/mnt/sharedata/ssd/common/LLMs/Yi-1.5-9B",
+        "YI-1.5-34B":"/mnt/sharedata/ssd/common/LLMs/Yi-1.5-34B",
+        "Yi-1.5-34B-Chat":"/mnt/sharedata/ssd/common/LLMs/Yi-1.5-34B-Chat",
+        "Ziya2-13B-Chat":"/mnt/sharedata/ssd/common/LLMs/Ziya2-13B-Chat",
+        "deepseek-llm-7b-chat":"/mnt/sharedata/ssd/common/LLMs/deepseek-llm-7b-chat",
+        "deepseek-llm-67b-chat":"/mnt/sharedata/ssd/common/LLMs/deepseek-llm-67b-chat",
+        "internlm2-7b":"/mnt/sharedata/ssd/common/LLMs/internlm2-7b",
+        "internlm2-chat-7b":"/mnt/sharedata/ssd/common/LLMs/internlm2-chat-7b",
+        "internlm2-20b":"/mnt/sharedata/ssd/common/LLMs/internlm2-20b",
+        "internlm2-chat-20b":"/mnt/sharedata/ssd/common/LLMs/internlm2-chat-20b",
+        "Baichuan2-13B-Chat":"/mnt/sharedata/ssd/common/LLMs/Baichuan2-13B-Chat",
+        "ShieldLM-13B-baichuan2":"/mnt/sharedata/ssd/common/LLMs/ShieldLM-13B-baichuan2",
+        "ShieldLM-6B-chatglm3":"/mnt/sharedata/ssd/common/LLMs/ShieldLM-6B-chatglm3",
+        "ShieldLM-7B-internlm2":"/mnt/sharedata/ssd/common/LLMs/ShieldLM-7B-internlm2",
+        "ShieldLM-14B-qwen":"/mnt/sharedata/ssd/common/LLMs/ShieldLM-14B-qwen",
+        }
 
 
 def main(args):
@@ -27,15 +77,15 @@ def main(args):
     # model = load_checkpoint_and_dispatch(
     #     model, "/mnt/sharedata/ssd/common/LLMs/Mixtral-8x22B-Instruct-v0.1", device_map="auto", dtype=torch.float16
     # )
-    model, tokenizer = get_model(args.pretrained_model_name), get_tokenizer(args.pretrained_model_name)
-    model.half()
+    # model, tokenizer = get_model(args.pretrained_model_name), get_tokenizer(args.pretrained_model_name)
+    model = outlines.models.transformers(pretrained_model_dic[args.pretrained_model_name])
 
     accuracys=[]
     precisions=[]
     recalls=[]
     precisions_neg=[]
     recalls_neg=[]
-    for seed in [100]:
+    for seed in [100,200,300]:
         setup_seed(seed)
 
         #####get data#####
@@ -56,10 +106,17 @@ def main(args):
 
 
         #####Inference#####
-        inferencer = get_inferencer('ppl', model_name=model, tokenizer_name = tokenizer, device = device, batch_size=args.batch_size)
-        test_predictions = inferencer.inference(task=args.task, ice=ice,  candidate=test_dataset['text'], labels=list(range(len(label))), ice_template=template_dict)
+        # inferencer = get_inferencer('gen', model_name=model, tokenizer_name = tokenizer, device = device, batch_size=args.batch_size)
+        # test_predictions = inferencer.inference(task=args.task, ice=ice,  candidate=test_dataset['text'], labels=list(range(len(label))), ice_template=template_dict)
         
         # print(delect_unavailable_word(test_predictions))
+
+        test_predictions=[]
+        for i in tqdm(range(len(test_dataset))):
+            generator = outlines.generate.choice(model, ["违规", "不违规"])
+            answer = generator(test_dataset['text'][i])
+            pred=0 if answer=="不违规" else 1
+            test_predictions.append(pred)
 
         #####Evaluate#####
         labels=test_dataset['label'].tolist()
@@ -76,7 +133,7 @@ def main(args):
         accuracys.append(acc)
 
         dat = pd.DataFrame({"preds":test_predictions, "label":test_dataset['label'], "ice":ice, "text":test_dataset['text']})
-        dat.to_csv('/data/home/huq/deepai/output_new/{model}_{ranking}_{ice_num}_{knn_num}_{task}_{dpp_candidate_num}_{test_retrieving}_{noise_retrieving}_{tau}_{seed}.csv'.format(model=args.pretrained_model_name ,ranking=args.ranking, tau=args.tau, dpp_candidate_num=args.dpp_candidate_num, ice_num=args.ice_num, task=args.task, test_retrieving=args.test_retrieving,  noise_retrieving=args.noise_retrieving, seed=seed,knn_num=args.knn_num))
+        dat.to_csv('/data/home/huq/deepai/output_gen/{model}_{ranking}_{ice_num}_{knn_num}_{task}_{dpp_candidate_num}_{test_retrieving}_{noise_retrieving}_{tau}_{seed}.csv'.format(model=args.pretrained_model_name ,ranking=args.ranking, tau=args.tau, dpp_candidate_num=args.dpp_candidate_num, ice_num=args.ice_num, task=args.task, test_retrieving=args.test_retrieving,  noise_retrieving=args.noise_retrieving, seed=seed,knn_num=args.knn_num))
 
     # dat = pd.DataFrame({"acc":acc})
     # dat.to_csv("/data/home/hongfugao/result/{task}_{icl}_{model}_{imbalance_ratio}.csv".format(task=args.task, icl=args.test_retrieving, model=args.pretrained_model_name, imbalance_ratio=args.imbalance_ratio))
@@ -85,7 +142,7 @@ def main(args):
     print(f"accuracy:{accuracys}, precision:{precisions}, recall:{recalls}, precision_n:{precisions_neg}, recall_n:{recalls_neg}")
     # resul = pd.DataFrame({"em": em, "rouge":rouge})
     resul = pd.DataFrame({"accuracy":accuracys, "precision": precisions, "recall":recalls, "precision_n":precisions_neg, "recall_n":recalls_neg})
-    resul.to_csv('/data/home/huq/deepai/output_new/result_{model}_{ranking}_{ice_num}_{knn_num}_{task}_{dpp_candidate_num}_{test_retrieving}_{noise_retrieving}_{tau}_{seed}.csv'.format(model=args.pretrained_model_name ,ranking=args.ranking, tau=args.tau, dpp_candidate_num=args.dpp_candidate_num, ice_num=args.ice_num, task=args.task, test_retrieving=args.test_retrieving,  noise_retrieving=args.noise_retrieving, seed=seed,knn_num=args.knn_num))
+    resul.to_csv('/data/home/huq/deepai/output_gen/result_{model}_{ranking}_{ice_num}_{knn_num}_{task}_{dpp_candidate_num}_{test_retrieving}_{noise_retrieving}_{tau}_{seed}.csv'.format(model=args.pretrained_model_name ,ranking=args.ranking, tau=args.tau, dpp_candidate_num=args.dpp_candidate_num, ice_num=args.ice_num, task=args.task, test_retrieving=args.test_retrieving,  noise_retrieving=args.noise_retrieving, seed=seed,knn_num=args.knn_num))
         
 
 
